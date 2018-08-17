327c315eedca15a0e5c34e75902b42aeb1b0a2fa
Vinicius Couto Espindola <vinicius.c.e@hotmail.com>
2018-08-16 22:36:59 -0300

    Linearized

--- aula_1.hs
+++ aula_1.hs
@@ -46,13 +46,68 @@ mergeList2 [] ys = ys
 mergeList2 (x:xs) (y:ys) = x:y:(mergeList2 xs ys)
 
 isSorted [x] = True
-isSorted (x:xs) = (x<=head xs) && isSorted xs
+isSorted (x:xs) = (x <= head xs) && isSorted xs
 
-createList 1 = [1]
-createList n = createList (n-1) ++ [n]
+createList n = createRecurse n 1
+createRecurse 1 acc = [acc]
+createRecurse n acc = acc:(createRecurse (n-1) (acc+1))
 
 getLast [x] = x
 getLast (x:xs) = getLast xs
 
 getInit [x] = []
 getInit (x:xs) = x:getInit xs
+
+shiftl (x:xs) = xs ++ [x]
+shiftr l =  getLast l:getInit l
+
+breakLeft 0 _ = []
+breakLeft _ [] = []
+breakLeft n (x:xs) = x:breakLeft (n-1) xs
+
+breakRight 0 l = l
+breakRight _ [] = []
+breakRight n (x:xs) = breakRight (n-1) xs
+
+shiftln n l = let
+  y = mod n (listLength l)
+  in breakRight y l ++ breakLeft y l
+
+shiftrn n l = let
+  leng = listLength l
+  y = 1 + mod (leng - (n + 1)) leng
+  in breakRight y l ++ breakLeft y l
+
+dumpOne _ [] = []
+dumpOne y (x:xs)
+  | (x==y) = xs
+  | otherwise = x:(dumpOne y xs)
+
+dumpAll _ [] = []
+dumpAll y (x:xs)
+  | (x==y) = dumpAll y xs
+  | otherwise = x:(dumpAll y xs)
+
+dumpNs 0 _ l = l
+dumpNs _ _ [] = []
+dumpNs n y (x:xs)
+  | (x==y) = dumpNs (n-1) y xs
+  | otherwise = x:dumpNs n y xs
+
+dumpLast y list = revertList (dumpOne y (revertList list))
+
+changeOne _ _ [] = []
+changeOne old new (x:xs)
+  | (x==old) = new:xs
+  | otherwise = x:changeOne old new xs
+
+changeAll _ _ [] = []
+changeAll old new (x:xs)
+  | (x==old) = new:changeAll old new xs
+  | otherwise = x:changeAll old new xs
+
+changeNs 0 _ _ l = l
+changeNs _ _ _ [] = []
+changeNs n old new (x:xs)
+  | (x==old) = new:changeNs (n-1) old new xs
+  | otherwise = x:changeNs n old new xs
