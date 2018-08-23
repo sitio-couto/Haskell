9a922be28158bbf257ea6ce07caeb492376ec67c
Vinicius Couto Espindola <vinicius.c.e@hotmail.com>
2018-08-23 08:52:37 -0300

    posicoes linearized

--- aula_3.hs
+++ aula_3.hs
@@ -3,9 +3,9 @@ str1 = "abcbcdcdefghabcdef"
 list = [1..10]
 list3 = [5,5,1,2,1,2,1,2,3,3,5]
 
-posicoes y list = posRecur y list 1
-  where posRecur _ [] _ = []
-        posRecur y list acc = [z | z <- [1..length list], list!!(z-1) == y]
+posicoes y list = pos y list 1
+  where pos _ [] _ = []
+        pos y (x:xs) acc = [z | z <- [acc], x==y]++[w | w <- pos y xs (acc+1)]
 
 -- split y list = splitRecur y list ""
 --   where splitRecur _ [] _ = []
