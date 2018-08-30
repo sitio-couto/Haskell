fe6eb234fa852bc235a1db651b5ef7b469c5f385
Vinicius Couto Espindola <vinicius.c.e@hotmail.com>
2018-08-29 22:39:37 -0300

    Added removeNode

--- aula_4.hs
+++ aula_4.hs
@@ -2,6 +2,8 @@
 root = (Node 5 (Node 3 (Node 2 Empty Empty) (Node 4 Empty Empty)) (Node 7 (Node 6 Empty Empty) (Node 8 Empty Empty)))
 root1 = Empty
 root2 = (Node 5 (Node 3 (Node 2 Empty Empty) (Node 4 Empty Empty)) (Node 7 (Node 6 (Node 0 Empty Empty) Empty) (Node 8 Empty Empty)))
+a = removeNode (removeNode root 2) 3
+
 
 data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Eq, Show, Read)
 
@@ -9,8 +11,23 @@ data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Eq, Show, Read)
 addNode :: (Ord a) => Tree a -> a -> Tree a
 addNode Empty new = (Node new Empty Empty)
 addNode (Node n tl td) new
-  | (new <= n) = (Node n (addNode tl new) td)
+  | (new == n) = (Node n tl td)
+  | (new < n) = (Node n (addNode tl new) td)
   | otherwise = (Node n tl (addNode td new))
+
+--Search node to remove from binary Tree
+removeNode :: (Ord a) => Tree a -> a -> Tree a
+removeNode Empty _ = Empty
+removeNode (Node n tl td) target
+  | (target == n) = removeRoot tl td
+  | (target < n) = (Node n (removeNode tl target) td)
+  | otherwise = (Node n tl (removeNode td target))
+
+--Removes root of tree and rearranges it
+removeRoot :: (Ord a) => Tree a -> Tree a -> Tree a
+removeRoot Empty Empty = Empty
+removeRoot (Node n tl td) sister = (Node n (removeRoot tl td) sister)
+removeRoot sister (Node n tl td) = (Node n sister (removeRoot tl td))
 
 --Search in binary Tree
 findNode :: (Ord a) => Tree a -> a -> Bool
