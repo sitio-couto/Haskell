9370f828bc71030fb60c93b3a4df17547836dd6c
Vinicius Couto Espindola <vinicius.c.e@hotmail.com>
2018-09-05 13:41:49 -0300

    transpose

--- aula_5.hs
+++ aula_5.hs
@@ -1,5 +1,5 @@
 
-
+mx = [[1,2,3],[4,5,6],[7,8,9],[0,0,-1]]
 list = [1..10]
 list2 = revertList list
 list3 = [5,5,1,2,1,2,1,2,3,3,5]
@@ -11,56 +11,36 @@ listLength l = foldl (\acc _ -> acc+1) 0 l
 
 sumAll l = foldl (+) 0 l
 
-sumPairIndex l = foldl (\acc (x,y) -> if mod x 2 == 0 then acc+y else acc) 0 (zip [1..length l] l)
+sumPairIndex l = foldl (\acc (x,y) -> if mod x 2 == 0 then acc+y else acc) 0 $ zip [1..length l] l
 
 isThere x l = [] /= filter (\y -> x==y) l
 
--- findPos i l = foldr (\(x,y) i -> if i==y then x else i) i (zip [1..length l] l)
+findPos i l = foldr (\(x,y) c -> if i==y then x:c else c) [] $ zip [1..length l] l
 
-countItem i l = foldr (+) 0 (map (\x -> if x==i then 1 else 0) l)
+countItem i l = foldr (\x c -> if i == x then c+1 else c) 0 l
 
 maxElem l = foldr (\x i -> if x > i then x else i) (head l) l
 
 revertList l = foldl (\i x -> x:i) [] l
 
-mergeList1 a b = foldr (\(x,y) i -> x:y:i) [] (zip a b)
+mergeList1 a b = foldr (\(x,y) i -> x:y:i) [] $ zip a b
 
 shorterVec a b = if length a < length b then b else a
-mergeList2 a b = (foldr (\(x,y) i -> x:y:i) [] (zip a b))++drop s l
+mergeList2 a b = (mergeList1 a b)++drop s l
                   where s = min (length a) (length b)
                         l = shorterVec a b
 
-isSorted l = foldl (\i (x,y) -> if x<=y then i&&True else False) True (pair l)
+isSorted l = foldl (\i (x,y) -> if x<=y then i&&True else False) True $ pair l
 pair l = zip l1 l2
          where l1 = init l
                l2 = tail l
 
-dumpAll i l = foldr (\(x,y) c -> if x==y then c else y:c) [] (map (\x -> (i,x)) l)
+dumpAll i l = foldr (\x c -> if x==i then c else x:c) [] l
 
-changeAll o n l = foldr (\(x,y) c -> if x==y then n:c else y:c) [] (map (\x -> (o,x)) l)
+changeAll o n l = foldr (\x c -> if x==o then n:c else x:c) [] l
 
-dumpOne _ [] = []
-dumpOne y (x:xs)
-  | (x==y) = xs
-  | otherwise = x:(dumpOne y xs)
+transpose m = foldr (\x c -> [x]:c ) [] m
 
 
-dumpNs 0 _ l = l
-dumpNs _ _ [] = []
-dumpNs n y (x:xs)
-  | (x==y) = dumpNs (n-1) y xs
-  | otherwise = x:dumpNs n y xs
 
-dumpLast y list = revertList (dumpOne y (revertList list))
-
-changeOne _ _ [] = []
-changeOne old new (x:xs)
-  | (x==old) = new:xs
-  | otherwise = x:changeOne old new xs
-
-
-changeNs 0 _ _ l = l
-changeNs _ _ _ [] = []
-changeNs n old new (x:xs)
-  | (x==old) = new:changeNs (n-1) old new xs
-  | otherwise = x:changeNs n old new xs
+-- transpose m = foldl (\(i,m) mt -> mt:foldr (\x c -> (x!!i):c) [] m) [] $ zip [0..length m] m
